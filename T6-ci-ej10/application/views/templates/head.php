03a8ae3207fc18bacf8110275e6d59d042a3f6cc
sergioortizalarcon <sergio.ortiz.alarcon2@gmail.com>
2016-11-24 22:57:57 +0100

    Update head.php

--- T6-ci-ej10/application/views/templates/head.php
+++ T6-ci-ej10/application/views/templates/head.php
@@ -12,6 +12,6 @@
 <script
 	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
         </script>
-<title>Cara o CRUD</title>
+<title>CRUD de empleados</title>
 </head>
 <body>
